d91d1066787dffb392f4016495502196636a69f8
Роман Кривошеев <krivosheev@amz.lan>
2022-05-04 13:53:57 +0300

    add test task process

---
+++ business-sector/src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import style from './Header.module.css';
+
+
+const HeaderContainer = (props) => {
+    return(
+        <header>
+            <h2 className={style.black}>Header</h2>
+        </header>
+    )
+}
+
+export default HeaderContainer
